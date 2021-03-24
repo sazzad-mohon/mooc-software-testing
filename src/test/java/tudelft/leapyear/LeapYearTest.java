946164c2d5cb94bd913a94ec78de9da7e4c9e50d
Sazzad Ahmed Mohon <sazzad.mohon@konasl.com>
2021-03-24 13:14:29 +0600

    Test Leapyears Combinations

--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
+
     @Test
     public void leapYearsThatAreNonCenturialYears() {
         LeapYear ly = new LeapYear();
